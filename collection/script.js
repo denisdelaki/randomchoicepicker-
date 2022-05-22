e6b041c7b6a6480daf9abe4088e8167b63eac740
denisdelaki <denisdelaki@gmail.com>
2022-05-22 10:40:58 +0300

    created an event listener  to add the values added to the text area and input them at the tags as choices

--- collection/script.js
+++ collection/script.js
@@ -2,3 +2,28 @@
 //declare a variable to hold the tags
 const taggi = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
+
+//use focus method to focus on the text area
+textarea.focus();
+//add anevent listerner
+textarea.addEventListener("keyup", (e) => {
+  //capture whatever is typed in the text area
+  createTags(e.target.value);
+});
+function createTags(input) {
+  //create a variable to store the splitted values and turn them into an array
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  taggi.innerHTML = "";
+  tags.forEach((tag) => {
+    //create a variable
+      const tagEl = document.createElement("span");
+      //add a class to the created element
+      tagEl.classList.add("tag");
+      //add text to the lement 
+    tagEl.innerText = tag;
+    taggi.appendChild(tagEl);
+  }); 
+}
