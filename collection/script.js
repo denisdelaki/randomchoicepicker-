cb81e304ab3738b61125e4d5e55c0d675548a29b
denisdelaki <denisdelaki@gmail.com>
2022-05-22 11:01:06 +0300

    created a function to randomly select the choices

--- collection/script.js
+++ collection/script.js
@@ -9,6 +9,15 @@ textarea.focus();
 textarea.addEventListener("keyup", (e) => {
   //capture whatever is typed in the text area
   createTags(e.target.value);
+  //create a condition to check when the eneter key isclicked
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      //clear the input value
+      e.target.value = " ";
+    }, 10);
+    //create a method to randomly select the choices
+    randomSelect();
+  }
 });
 function createTags(input) {
   //create a variable to store the splitted values and turn them into an array
@@ -19,11 +28,18 @@ function createTags(input) {
   taggi.innerHTML = "";
   tags.forEach((tag) => {
     //create a variable
-      const tagEl = document.createElement("span");
-      //add a class to the created element
-      tagEl.classList.add("tag");
-      //add text to the lement 
+    const tagEl = document.createElement("span");
+    //add a class to the created element
+    tagEl.classList.add("tag");
+    //add text to the lement
     tagEl.innerText = tag;
     taggi.appendChild(tagEl);
-  }); 
+  });
+}
+//create a random select function
+function randomSelect() {
+    // the the time to 30 secs
+    const times = 30
+    //create a variable to store the interval and use the interval function 
+    
 }
