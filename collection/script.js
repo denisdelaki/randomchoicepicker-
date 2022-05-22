3b3c5d92b966c68bf69ee6c1e06d0a3018097183
denisdelaki <denisdelaki@gmail.com>
2022-05-22 09:59:26 +0300

    create the variables to store the text area and the tags

---
+++ collection/script.js
@@ -0,0 +1,4 @@
+//we want to get  the tags into the text area
+//declare a variable to hold the tags
+const taggi = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
